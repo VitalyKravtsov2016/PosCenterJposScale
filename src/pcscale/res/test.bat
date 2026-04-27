524d2489b3dde2ee13f863313ce2ab3da294c901
Vitaly Kravtsov <vitalykravtsov@gmail.com>
2026-04-27 17:13:07 +0300

    [-] Исправлена сборка

--- src/pcscale/res/test.bat
+++ src/pcscale/res/test.bat
@@ -1,7 +1,7 @@
 @echo off
 
-java -cp .;pcscale-1.14.2.jar;javapos-1.14.2.jar;log4j-api-2.25.1.jar;log4j-core-2.25.1.jar;nrjavaserial-3.9.3.jar;xercesImpl-2.12.2.jar -Djava.library.path=. ru.poscenter.ScaleTests
-REM java -cp .;pcscale-1.14.2.jar;javapos-1.14.2.jar;log4j-api-2.25.1.jar;log4j-core-2.25.1.jar;nrjavaserial-3.9.3.jar;xercesImpl-2.12.2.jar -Djava.library.path=. ru.poscenter.scaletst.ScaleTest
-REM java -cp .;pcscale-1.14.2.jar;javapos-1.14.2.jar;log4j-api-2.25.1.jar;log4j-core-2.25.1.jar;nrjavaserial-3.9.3.jar;xercesImpl-2.12.2.jar -Djava.library.path=. ru.poscenter.scalecalib.MainDialog
-REM java -cp .;pcscale-1.14.2.jar;javapos-1.14.2.jar;log4j-api-2.25.1.jar;log4j-core-2.25.1.jar;nrjavaserial-3.9.3.jar;xercesImpl-2.12.2.jar -Djava.library.path=. ru.poscenter.test.ConsoleTest
-REM java -cp .;pcscale-1.14.2.jar;javapos-1.14.2.jar;log4j-api-2.25.1.jar;log4j-core-2.25.1.jar;nrjavaserial-3.9.3.jar;xercesImpl-2.12.2.jar -Djava.library.path=. ru.poscenter.ScaleCLI
+java -cp .;pcscale-1.14.4.jar;javapos-1.14.2.jar;log4j-api-2.25.1.jar;log4j-core-2.25.1.jar;nrjavaserial-3.9.3.jar;xercesImpl-2.12.2.jar -Djava.library.path=. ru.poscenter.jpostest.JposScaleTestApp
+REM java -cp .;pcscale-1.14.4.jar;javapos-1.14.2.jar;log4j-api-2.25.1.jar;log4j-core-2.25.1.jar;nrjavaserial-3.9.3.jar;xercesImpl-2.12.2.jar -Djava.library.path=. ru.poscenter.scaletst.ScaleTest
+REM java -cp .;pcscale-1.14.4.jar;javapos-1.14.2.jar;log4j-api-2.25.1.jar;log4j-core-2.25.1.jar;nrjavaserial-3.9.3.jar;xercesImpl-2.12.2.jar -Djava.library.path=. ru.poscenter.scalecalib.MainDialog
+REM java -cp .;pcscale-1.14.4.jar;javapos-1.14.2.jar;log4j-api-2.25.1.jar;log4j-core-2.25.1.jar;nrjavaserial-3.9.3.jar;xercesImpl-2.12.2.jar -Djava.library.path=. ru.poscenter.test.ConsoleTest
+REM java -cp .;pcscale-1.14.4.jar;javapos-1.14.2.jar;log4j-api-2.25.1.jar;log4j-core-2.25.1.jar;nrjavaserial-3.9.3.jar;xercesImpl-2.12.2.jar -Djava.library.path=. ru.poscenter.ScaleCLI
--- src/pcscale/src/ru/poscenter/util/ServiceVersion.java
+++ src/pcscale/src/ru/poscenter/util/ServiceVersion.java
@@ -1,5 +1,5 @@
 package ru.poscenter.util;
 
 public class ServiceVersion {
-    public static final String VERSION = "1.14.3";
+    public static final String VERSION = "1.14.4";
 }
